b2c710aad21a1ed2388b2d484e91b1fad5e37138
cembora19 <cemboraceylandev@gmail.com>
2026-01-12 18:43:10 +0300

    Initial solution structure with API, layers and ConsoleUI

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,10 @@
+namespace Entities.Concrete;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public int CategoryId { get; set; }
+    public string ProductName { get; set; }
+    public short UnitInStock { get; set; }
+    public decimal UnitPrice { get; set; }
+}
